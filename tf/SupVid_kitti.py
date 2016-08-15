59ad9bbc16905edfed0d245206b996a01074136b
Sheng Lundquist <shenglundquist@gmail.com>
2016-08-15 14:37:29 -0600

    Added supervised kitti

---
+++ tf/SupVid_kitti.py
@@ -0,0 +1,291 @@
+import pdb
+import numpy as np
+import tensorflow as tf
+from loadVgg import loadWeights
+from utils import *
+import os
+from plot.viewCam import plotDetCam
+from base import TFObj
+import scipy.sparse as sp
+#import matplotlib.pyplot as plt
+
+class SupVid_kitti(TFObj):
+
+    #Sets dictionary of params to member variables
+    def loadParams(self, params):
+        super(SupVid_kitti, self).loadParams(params)
+
+        self.beta1 = params['beta1']
+        self.beta2 = params['beta2']
+        self.epsilon = params['epsilon']
+        self.learningRateBias = params['learningRateBias']
+        self.lossWeight = params['lossWeight']
+        self.gtShape = params['gtShape']
+        self.gtSparse = params['gtSparse']
+
+    #Builds the model. inMatFilename should be the vgg file
+    def buildModel(self, inputShape):
+        #Running on GPU
+        with tf.device(self.device):
+            with tf.name_scope("inputOps"):
+                #self.inputImage = node_variable([self.batchSize, inputShape[0], inputShape[1], inputShape[2], inputShape[3]], "inputImage")
+                #self.gt = node_variable([self.batchSize, 1, 8, 16, self.numClasses], "gt")
+
+                self.inputImage = node_variable((self.batchSize,)+inputShape, "inputImage")
+                #We split the time dimension to stereo and concatenate with feature dim
+                self.reshapeImage = tf.reshape(self.inputImage,
+                        [self.batchSize, 3, 2, inputShape[1], inputShape[2], inputShape[3]])
+                self.permuteImage = tf.transpose(self.reshapeImage, [0, 1, 3, 4, 5, 2])
+                self.stereoImage = tf.reshape(self.permuteImage,
+                        [self.batchSize, 3, inputShape[1], inputShape[2], inputShape[3]*2])
+                self.padInput = tf.pad(self.stereoImage, [[0, 0], [0, 0], [7, 7], [15, 15], [0, 0]])
+                #Reshape time dimension to feature dimension
+
+
+
+                if(self.gtSparse):
+                    self.gtIndices = tf.placeholder("int64", [2, None], "gtIndices")
+                    self.gtValues = node_variable([None], "gtValues")
+
+                    self.pre_gt = tf.sparse_tensor_to_dense(tf.SparseTensor(
+                            tf.transpose(self.gtIndices, [1, 0]),
+                            self.gtValues,
+                            [self.batchSize*self.gtShape[0], self.gtShape[1]*self.gtShape[2]*self.gtShape[3]]),
+                            validate_indices=False
+                            )
+                    self.gt = tf.reshape(self.pre_gt, [self.batchSize, self.gtShape[0], self.gtShape[1], self.gtShape[2], self.gtShape[3]])
+                else:
+                    self.gt=tf.placeholder("float32", [self.batchSize, self.gtShape[0], self.gtShape[1], self.gtShape[2], self.gtShape[3]])
+
+                self.select_gt = tf.squeeze(self.gt[:, :, :, :, 0:self.numClasses], squeeze_dims=[1])
+
+                #self.norm_gt = self.gt/tf.reduce_sum(self.gt, reduction_indices=4, keep_dims=True)
+
+            with tf.name_scope("Hidden"):
+                self.h_weight = weight_variable_xavier([2, 16, 32, 6, 3072], "hidden_weight")
+                self.h_bias = bias_variable([3072], "hidden_bias")
+                self.h_hidden= tf.nn.relu(tf.nn.conv3d(self.padInput, self.h_weight, [1, 1, 4, 4, 1], padding="VALID") + self.h_bias)
+                #self.training = tf.placeholder("bool", name="training")
+                #(self.h_norm_hidden, self.beta, self.gamma) = standard_batch_norm("hidden", self.h_hidden, 3072, self.training)
+
+            with tf.name_scope("Pool"):
+                #Pool over spatial dimensions to be 2x2
+                #self.inputPooled = tf.nn.max_pool3d(self.h_norm_hidden, ksize=[1, 4, 8, 8, 1], strides=[1, 4, 8, 8, 1], padding="SAME")
+                #self.camPooled = tf.nn.max_pool3d(self.h_norm_hidden, ksize=[1, 4, 8, 8, 1], strides=[1, 4, 1, 1, 1], padding="SAME")
+
+                yPool = int(np.ceil(float(16)/self.gtShape[1]))
+                xPool = int(np.ceil(float(64)/self.gtShape[2]))
+
+                self.inputPooled = tf.nn.max_pool3d(self.h_hidden, ksize=[1, 2, yPool, xPool, 1], strides=[1, 2, yPool, xPool, 1], padding="SAME")
+                self.camPooled = tf.nn.max_pool3d(self.h_hidden, ksize=[1, 2, yPool, xPool, 1], strides=[1, 2, 1, 1, 1], padding="SAME")
+
+                self.weight = weight_variable_xavier([1, 1, 3072, self.numClasses], "weight")
+                self.bias = bias_variable([self.numClasses], "bias" )
+
+                self.r_inputPooled = tf.squeeze(self.inputPooled, squeeze_dims=[1])
+                self.r_camPooled = tf.squeeze(self.camPooled, squeeze_dims=[1])
+
+                self.h_conv = tf.nn.conv2d(self.r_inputPooled, self.weight, [1, 1, 1, 1], padding="VALID") + self.bias
+                self.cam = tf.nn.conv2d(self.r_camPooled, self.weight, [1, 1, 1, 1], padding="VALID") + self.bias
+
+                #Reshape batch and time together
+                #self.reshape_cam = tf.transpose(tf.reshape(self.cam, [self.batchSize*7, 16, 32, 31]), [0, 3, 1, 2])
+                self.reshape_cam = tf.transpose(self.cam, [0, 3, 1, 2])
+
+                #Get ranking from h_conv
+                self.classRank = tf.reduce_mean(self.reshape_cam, reduction_indices=[2, 3])
+
+                self.est = pixelSoftmax(self.h_conv)
+                #self.est = self.h_conv
+
+            with tf.name_scope("Loss"):
+                self.flat_gt = tf.reshape(self.select_gt, [-1, self.numClasses])
+                self.flat_est = tf.reshape(self.est, [-1, self.numClasses])
+
+                gtClass = tf.argmax(self.flat_gt, 1)
+                estClass = tf.argmax(self.flat_est, 1)
+                correct = tf.equal(gtClass, estClass)
+                self.accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+                self.classF1 = []
+                for c in range(self.numClasses):
+                    classGT = tf.equal(gtClass, c)
+                    classEst = tf.equal(estClass, c)
+                    classTP = tf.reduce_sum(tf.cast(tf.logical_and(classGT, classEst), tf.float32))
+                    classFP = tf.reduce_sum(tf.cast(tf.logical_and(tf.logical_not(classGT), classEst), tf.float32))
+                    classFN = tf.reduce_sum(tf.cast(tf.logical_and(classGT, tf.logical_not(classEst)), tf.float32))
+
+                    precision = classTP/(classTP+classFP+self.epsilon)
+                    recall = classTP/(classTP+classFN+self.epsilon)
+                    self.classF1.append((2*precision*recall)/(precision+recall+self.epsilon))
+
+                self.loss = tf.reduce_mean(-tf.reduce_sum(self.lossWeight[0:self.numClasses] * self.select_gt* tf.log(self.est+self.epsilon), reduction_indices=3))
+                #self.loss = 0.5 * tf.reduce_mean(tf.reduce_sum(tf.square(self.gt - self.est), reduction_indices=[1, 2, 3, 4]))
+
+            with tf.name_scope("Opt"):
+                self.optimizerAll = tf.train.AdamOptimizer(self.learningRate, beta1=self.beta1, beta2=self.beta2, epsilon=self.epsilon).minimize(self.loss,
+                        var_list=[
+                            self.h_weight,
+                            #self.beta,
+                            #self.gamma,
+                            self.weight,
+                        ]
+                        )
+                self.optimizerBias = tf.train.GradientDescentOptimizer(self.learningRateBias).minimize(self.loss,
+                        var_list=[
+                            self.h_bias,
+                            self.bias,
+                        ]
+                        )
+
+                self.optimizerPre = tf.train.AdamOptimizer(self.learningRate, beta1=self.beta1, beta2=self.beta2, epsilon=self.epsilon).minimize(self.loss,
+                        var_list=[
+                            self.weight,
+                        ]
+                        )
+                self.optimizerPreBias = tf.train.GradientDescentOptimizer(self.learningRateBias).minimize(self.loss,
+                        var_list=[
+                            self.bias,
+                        ]
+                        )
+
+        (self.eval_vals, self.eval_idx) = tf.nn.top_k(self.classRank, k=5)
+
+        #Summaries
+        tf.scalar_summary('loss', self.loss, name="accuracy")
+        tf.scalar_summary('accuracy', self.accuracy, name="accuracy")
+        for c in range(self.numClasses):
+            className = self.idxToName[c]
+            tf.scalar_summary(className+' F1', self.classF1[c])
+
+        tf.histogram_summary('input', self.inputImage, name="image_vis")
+        tf.histogram_summary('inputPooled', self.inputPooled, name="image_vis")
+        tf.histogram_summary('gt', self.select_gt, name="gt_vis")
+        #Conv layer histograms
+        tf.histogram_summary('h_conv', self.h_conv, name="conv1_vis")
+        tf.histogram_summary('h_hidden', self.h_hidden, name="hidden_vis")
+        tf.histogram_summary('h_norm_hidden', self.h_hidden, name="norm_hidden_vis")
+        tf.histogram_summary('est', self.est, name="est_vis")
+        #Weight and bias hists
+        tf.histogram_summary('h_weight', self.h_weight, name="weight_vis")
+        tf.histogram_summary('h_bias', self.h_bias, name="bias_vis")
+        #tf.histogram_summary('h_beta', self.beta, name="beta_vis")
+        #tf.histogram_summary('h_gamma', self.gamma, name="gamma_vis")
+        tf.histogram_summary('weight', self.weight, name="weight_vis")
+        tf.histogram_summary('bias', self.bias, name="bias_vis")
+
+
+    def getLoadVars(self):
+        v = tf.all_variables()
+        return v
+
+    #Trains model for numSteps
+    #If pre is False, will train entire network
+    #If pre is True, will train only fully connected network
+    def trainModel(self, dataObj, save, plot):
+        for i in range(self.innerSteps):
+            #Get data from dataObj
+            data = dataObj.getData(self.batchSize)
+            if(self.gtSparse):
+                (gtOutY, gtOutX, gtVals) = sp.find(data[1])
+                feedDict = {self.inputImage:data[0],
+                            self.gtIndices:[gtOutY, gtOutX], self.gtValues:gtVals
+                            }
+            else:
+                feedDict = {self.inputImage:data[0],
+                            self.gt:data[1]
+                            }
+
+            #feedDict = {self.inputImage: data[0], self.gt: data[1]}
+            #Run optimizer
+            if(self.preTrain):
+                self.sess.run(self.optimizerPre, feed_dict=feedDict)
+                self.sess.run(self.optimizerPreBias, feed_dict=feedDict)
+            else:
+                self.sess.run(self.optimizerAll, feed_dict=feedDict)
+                self.sess.run(self.optimizerBias, feed_dict=feedDict)
+
+            if(i%self.writeStep == 0):
+                summary = self.sess.run(self.mergedSummary, feed_dict=feedDict)
+                self.train_writer.add_summary(summary, self.timestep)
+            if(i%self.progress == 0):
+                print "Timestep ", self.timestep
+            self.timestep+=1
+        if(save):
+            save_path = self.saver.save(self.sess, self.saveFile, global_step=self.timestep, write_meta_graph=False)
+            print("Model saved in file: %s" % save_path)
+        if(plot):
+            filename = self.plotDir + "train_" + str(self.timestep)
+            gtShape = dataObj.gtShape
+            if(self.gtSparse):
+                gt = np.reshape(data[1].toarray(), (self.batchSize, gtShape[0], gtShape[1], gtShape[2], gtShape[3]))
+            else:
+                gt = data[1]
+            self.evalAndPlotCam(feedDict, data, gt, filename)
+
+    def evalAndPlotCam(self, feedDict, data, gt, prefix):
+        print "Plotting"
+
+        #We need feed_dict here
+        cam = self.sess.run(self.reshape_cam, feed_dict=feedDict)
+
+        img = data[2]
+        camIdxs = self.sess.run(self.eval_idx, feed_dict=feedDict)
+        camVals = self.sess.run(self.eval_vals, feed_dict=feedDict)
+        #We conlidate the time and batch dim into 1
+        (batch, imgT, imgY, imgX, imgF) = img.shape
+        (batch, gtT, gtY, gtX, gtF) = gt.shape
+        reshape_img = np.reshape(img, [batch*imgT, imgY, imgX, imgF])
+        reshape_gt = np.reshape(gt, [batch*gtT, gtY, gtX, gtF])
+
+        plotDetCam(prefix, reshape_img, reshape_gt, cam, camIdxs, camVals, self.idxToName, distIdx=0)
+
+    def runModel(self, trainDataObj, testDataObj=None):
+        for i in range(self.outerSteps):
+           #Plot flag
+           if(i%self.plotPeriod == 0):
+               plot = True
+           else:
+               plot = False
+           if(testDataObj):
+               #Evaluate test frame, providing gt so that it writes to summary
+               (evalData, gtData, imgData) = testDataObj.getData(self.batchSize)
+               self.evalModel(evalData, gtData, imgData, testDataObj.gtShape, plot=plot)
+               print "Done test eval"
+           #Train
+           if(i%self.savePeriod == 0):
+               self.trainModel(trainDataObj, save=True, plot=plot)
+           else:
+               self.trainModel(trainDataObj, save=False, plot=plot)
+
+    #Evaluates all of inData at once
+    #If an inGt is provided, will calculate summary as test set
+    def evalModel(self, inData, inGt, inImg, gtShape, plot=True):
+
+        if(inGt != None):
+            if(self.gtSparse):
+                (gtOutY, gtOutX, gtVals) = sp.find(inGt)
+                feedDict = {self.inputImage:inData,
+                            self.gtIndices:[gtOutY, gtOutX], self.gtValues:gtVals,
+                            }
+            else:
+                feedDict = {self.inputImage:inData,
+                        self.gt:inGt}
+        else:
+            feedDict ={self.inputImage:inData}
+
+        outVals = self.est.eval(feed_dict=feedDict, session=self.sess)
+        if(inGt != None):
+            summary = self.sess.run(self.mergedSummary, feed_dict=feedDict)
+            self.test_writer.add_summary(summary, self.timestep)
+        if(plot):
+            filename = self.plotDir + "test_" + str(self.timestep)
+            if(self.gtSparse):
+                gt = np.reshape(inGt.toarray(), (self.batchSize, gtShape[0], gtShape[1], gtShape[2], gtShape[3]))
+            else:
+                gt = inGt
+            data = (inData, inGt, inImg)
+            self.evalAndPlotCam(feedDict, data, gt, filename)
+
+        return outVals
+
---
+++ train_sup_kitti_vid.py
@@ -0,0 +1,89 @@
+import matplotlib
+matplotlib.use('Agg')
+#import matplotlib.pyplot as plt
+from dataObj.pv_image import kittiVidPvObj
+from tf.SupVid_kitti import SupVid_kitti
+import numpy as np
+import pdb
+
+#Paths to list of filenames
+#Since we reshape from 6 to 3x2 (3 time, 2 stereo), left/right spin fastest
+trainInputs = [
+            "/home/slundquist/mountData/kitti_pv/objdet_train2/FrameLeft0.pvp",
+            "/home/slundquist/mountData/kitti_pv/objdet_train2/FrameRight0.pvp",
+            "/home/slundquist/mountData/kitti_pv/objdet_train2/FrameLeft1.pvp",
+            "/home/slundquist/mountData/kitti_pv/objdet_train2/FrameRight1.pvp",
+            "/home/slundquist/mountData/kitti_pv/objdet_train2/FrameLeft2.pvp",
+            "/home/slundquist/mountData/kitti_pv/objdet_train2/FrameRight2.pvp",
+            ]
+
+trainGts = [
+            "/home/slundquist/mountData/kitti_pv/objdet_train2/GroundTruth2Background.pvp",
+        ]
+trainFilenames = [
+            "/home/slundquist/mountData/kitti_pv/objdet_train2/FrameLeft2.pvp",
+        ]
+
+dncFilenames= [
+            "/home/slundquist/mountData/kitti_pv/objdet_train2/DNCPixels2.pvp",
+        ]
+
+#trainFnPrefix = "/shared/KITTI/objdet/training/"
+
+#Get object from which tensorflow will pull data from
+trainDataObj = kittiVidPvObj(trainInputs, trainGts, trainFilenames, dncFilenames, None, shuffle=True, startIdx = 0, stopIdx = 6000)
+testDataObj = kittiVidPvObj(trainInputs, trainGts, trainFilenames, dncFilenames, None, shuffle=True, startIdx=6000, stopIdx=-1)
+
+params = {
+    #Base output directory
+    'outDir':          "/home/slundquist/mountData/DeepGAP/",
+    #Inner run directory
+    'runDir':          "/sup_kitti_vid_4x8/",
+    'tfDir':           "/tfout",
+    #Save parameters
+    'ckptDir':         "/checkpoints/",
+    'saveFile':        "/save-model",
+    'savePeriod':      10, #In terms of displayPeriod
+    #output plots directory
+    'plotDir':         "plots/",
+    'plotPeriod':      10, #With respect to displayPeriod
+    #Progress step
+    'progress':        1,
+    #Controls how often to write out to tensorboard
+    'writeStep':       50, #300,
+    #Flag for loading weights from checkpoint
+    'load':            False,
+    'loadFile':        "/home/slundquist/mountData/DeepGAP/saved/sup_imagenet_vid_2x4.ckpt",
+    #Device to run on
+    'device':          '/gpu:0',
+    #####ISTA PARAMS######
+    #Num iterations
+    'outerSteps':      10000000, #1000000,
+    'innerSteps':      1, #300,
+    #Batch size
+    'batchSize':       16,
+    #Learning rate for optimizer
+    'learningRate':    1e-4,
+    'beta1' :          .9,
+    'beta2' :          .999,
+    'epsilon':         1e-8,
+    'learningRateBias': 1e-6,
+    #'numClasses': trainDataObj.numClasses,
+    'numClasses': trainDataObj.numClasses,
+    'idxToName': trainDataObj.idxToName,
+    'preTrain': False,
+    'lossWeight': trainDataObj.lossWeight,
+    'gtShape': trainDataObj.gtShape,
+    'gtSparse': False
+}
+
+#Allocate tensorflow object
+#This will build the graph
+tfObj = SupVid_kitti(params, trainDataObj.inputShape)
+
+print "Done init"
+tfObj.runModel(trainDataObj, testDataObj = testDataObj)
+print "Done run"
+
+tfObj.closeSess()
+
